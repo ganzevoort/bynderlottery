27a73fb9c8e4eb2c7e0b891dd55324b9655a36ee
Job Ganzevoort <ganzevoort@gw20e.com>
2025-07-26 08:46:58 +0200

    HTML templates for e-mail
    - nicely formatted emails
    - added a logo to show of cid: inlined images

--- TODO.md
+++ TODO.md
@@ -1,13 +1,11 @@
 # general
 - monitoring
-- logo
 
 # accounts
 - captchas
 - 2fa
 - monitoring metrics: #accounts, #unverified etc
 - remove old unverified accounts?
-- email html templates
 
 # lottery
 - payout for winners
--- backend/service/settings/environment.py
+++ backend/service/settings/environment.py
@@ -12,6 +12,11 @@ __all__ = [
     "DATABASE_USER",
     "DATABASE_PASSWORD",
     "EMAIL_BACKEND",
+    "EMAIL_HOST",
+    "EMAIL_PORT",
+    "EMAIL_USE_TLS",
+    "EMAIL_HOST_USER",
+    "EMAIL_HOST_PASSWORD",
     "DEFAULT_FROM_EMAIL",
     "TIME_ZONE",
 ]
